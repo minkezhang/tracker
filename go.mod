aa897ae97a5d8178d4a6933c9308a3990f913a2e
Minke Zhang <minke.zhang@gmail.com>
2022-05-23 21:34:55 -0700

    Add Golang generated protobufsg

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/minkezhang/tracker
 
 go 1.18
+
+require google.golang.org/protobuf v1.28.0
---
+++ tools/importer/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	dpb "github.com/minkezhang/tracker/api/go/database"
+)
+
+func main() {
+	panic(dpb.Entry{})
+}
