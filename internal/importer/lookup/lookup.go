159ad41d9a7c6e3c44d2090424a08b3c0968874f
Minke Zhang <minke.zhang@gmail.com>
2022-05-24 00:31:47 -0700

    Update custom data importer to include all fields

---
+++ internal/importer/lookup/lookup.go
@@ -0,0 +1,34 @@
+package lookup
+
+import (
+	dpb "github.com/minkezhang/tracker/api/go/database"
+)
+
+var (
+	Corpus = map[string]dpb.Corpus{
+		"Anime":       dpb.Corpus_CORPUS_ANIME,
+		"Anime Film":  dpb.Corpus_CORPUS_ANIME_FILM,
+		"Manga":       dpb.Corpus_CORPUS_MANGA,
+		"Book":        dpb.Corpus_CORPUS_BOOK,
+		"TV Show":     dpb.Corpus_CORPUS_TV,
+		"Film":        dpb.Corpus_CORPUS_FILM,
+		"Game":        dpb.Corpus_CORPUS_GAME,
+		"Album":       dpb.Corpus_CORPUS_ALBUM,
+		"Short Story": dpb.Corpus_CORPUS_SHORT_STORY,
+	}
+
+	Provider = map[string]dpb.Provider{
+		"Crunchyroll":      dpb.Provider_PROVIDER_CRUNCHYROLL,
+		"Netflix":          dpb.Provider_PROVIDER_NETFLIX,
+		"Amazon Streaming": dpb.Provider_PROVIDER_AMAZON_STREAMING,
+		"Battlenet":        dpb.Provider_PROVIDER_BATTLENET,
+		"Epic":             dpb.Provider_PROVIDER_EPIC,
+		"Google Play":      dpb.Provider_PROVIDER_GOOGLE_PLAY,
+		"Hulu":             dpb.Provider_PROVIDER_HULU,
+		"Origins":          dpb.Provider_PROVIDER_ORIGINS,
+		"Steam":            dpb.Provider_PROVIDER_STEAM,
+		"Funimation":       dpb.Provider_PROVIDER_FUNIMATION,
+		"Disney+":          dpb.Provider_PROVIDER_DISNEY_PLUS,
+		"HBO Max":          dpb.Provider_PROVIDER_HBO_MAX,
+	}
+)
--- internal/importer/main.go
+++ internal/importer/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"bufio"
+	"encoding/csv"
 	"flag"
 	"io"
 	"log"
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/minkezhang/tracker/internal/importer/lookup"
 	"google.golang.org/protobuf/encoding/prototext"
 
 	dpb "github.com/minkezhang/tracker/api/go/database"
@@ -18,25 +19,32 @@ import (
 var (
 	input  = flag.String("input", "/dev/stdin", "input CSV path, e.g. path/to/database.csv")
 	output = flag.String("output", "/dev/stdout", "output textproto path, e.g. path/to/database.textproto")
-	foo    = flag.String("f", "abc", "")
 )
 
 type E [11]string
 
-func (r E) Corpus() dpb.Corpus {
-	return map[string]dpb.Corpus{
-		"Anime":      dpb.Corpus_CORPUS_ANIME,
-		"Anime Film": dpb.Corpus_CORPUS_ANIME_FILM,
-		"Manga":      dpb.Corpus_CORPUS_MANGA,
-	}[r[8]]
-}
-
+func (r E) Corpus() dpb.Corpus  { return lookup.Corpus[r[8]] }
 func (r E) Title() string       { return r[0] }
 func (r E) Queued() bool        { return r[2] == "TRUE" }
-func (r E) Directors() []string { return strings.Split(r[5], ",") }
-func (r E) Writers() []string   { return strings.Split(r[6], ",") }
-func (r E) Author() string      { return r[6] }
+func (r E) Composers() []string { return r.Writers() }
+func (r E) Authors() []string   { return r.Writers() }
 func (r E) Studios() []string   { return []string{r[7]} }
+
+func (r E) Directors() []string {
+	var directors []string
+	for _, v := range strings.Split(r[5], ",") {
+		directors = append(directors, strings.Trim(v, " "))
+	}
+	return directors
+}
+
+func (r E) Writers() []string {
+	var writers []string
+	for _, v := range strings.Split(r[6], ",") {
+		writers = append(writers, strings.Trim(v, " "))
+	}
+	return writers
+}
 
 func (r E) Score() float32 {
 	if f, err := strconv.ParseFloat(r[3], 32); err != nil {
@@ -47,13 +55,9 @@ func (r E) Score() float32 {
 }
 
 func (r E) Providers() []dpb.Provider {
-	lookup := map[string]dpb.Provider{
-		"Crunchyroll": dpb.Provider_PROVIDER_CRUNCHYROLL,
-		"Netflix":     dpb.Provider_PROVIDER_NETFLIX,
-	}
 	var dedupe = map[dpb.Provider]bool{}
-	dedupe[lookup[r[9]]] = true
-	dedupe[lookup[r[10]]] = true
+	dedupe[lookup.Provider[r[9]]] = true
+	dedupe[lookup.Provider[r[10]]] = true
 
 	var providers []dpb.Provider
 	for k := range dedupe {
@@ -120,10 +124,15 @@ func (r E) ProtoBuf() *dpb.Entry {
 		Providers: r.Providers(),
 	}
 	switch e.GetCorpus() {
+
+	case dpb.Corpus_CORPUS_TV:
+		fallthrough
 	case dpb.Corpus_CORPUS_ANIME:
 		e.Tracker = &dpb.Entry_TrackerVideo{
 			TrackerVideo: r.TrackerVideo(),
 		}
+		fallthrough
+	case dpb.Corpus_CORPUS_FILM:
 		fallthrough
 	case dpb.Corpus_CORPUS_ANIME_FILM:
 		e.AuxData = &dpb.Entry_AuxDataVideo{
@@ -133,13 +142,34 @@ func (r E) ProtoBuf() *dpb.Entry {
 				Writers:   r.Writers(),
 			},
 		}
+
+	case dpb.Corpus_CORPUS_GAME:
+		e.AuxData = &dpb.Entry_AuxDataGame{
+			AuxDataGame: &dpb.AuxDataGame{
+				Studios:   r.Studios(),
+				Directors: r.Directors(),
+				Writers:   r.Writers(),
+			},
+		}
+
 	case dpb.Corpus_CORPUS_MANGA:
+		fallthrough
+	case dpb.Corpus_CORPUS_BOOK:
 		e.Tracker = &dpb.Entry_TrackerBook{
 			TrackerBook: r.TrackerBook(),
 		}
+		fallthrough
+	case dpb.Corpus_CORPUS_SHORT_STORY:
 		e.AuxData = &dpb.Entry_AuxDataBook{
 			AuxDataBook: &dpb.AuxDataBook{
-				Author: r.Author(),
+				Authors: r.Authors(),
+			},
+		}
+
+	case dpb.Corpus_CORPUS_ALBUM:
+		e.AuxData = &dpb.Entry_AuxDataAudio{
+			AuxDataAudio: &dpb.AuxDataAudio{
+				Composers: r.Composers(),
 			},
 		}
 	default:
@@ -156,21 +186,21 @@ func main() {
 	}
 	defer fp.Close()
 
-	scanner := bufio.NewReader(fp)
-	if _, err := scanner.ReadString('\n'); err != nil {
-		log.Fatalf("error while reading file %v: %v", *input, err)
+	scanner := csv.NewReader(fp)
+	if _, err := scanner.Read(); err != nil {
+		log.Fatalf("error while reading CSV file %v: %v", *input, err)
 	}
 
 	var entries []*dpb.Entry
-	for l, err := scanner.ReadString('\n'); err == nil; l, err = scanner.ReadString('\n') {
-		r := (*E)(strings.Split(l, ","))
+	for l, err := scanner.Read(); err == nil; l, err = scanner.Read() {
+		r := (*E)(l)
 		if e := r.ProtoBuf(); e.Corpus != dpb.Corpus_CORPUS_UNKNOWN {
 			entries = append(entries, e)
 		}
 	}
 
 	if err != nil && err != io.EOF {
-		log.Fatalf("error while reading file %v: %v", *input, err)
+		log.Fatalf("error while reading CSV file %v: %v", *input, err)
 	}
 
 	db := &dpb.Database{
