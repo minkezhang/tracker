aac28a3d6e4acc841a051c957916b2b8a2018e87
Minke Zhang <minke.zhang@gmail.com>
2022-05-24 17:29:49 -0700

    Move internal tools to public tools

---
+++ data/formats/minkezhang/columns/columns.go
@@ -0,0 +1,17 @@
+package columns
+
+type C int
+
+const (
+	Title C = iota
+	Completed
+	Queued
+	Rating
+	Bookmark
+	Directors
+	Writers
+	Studio
+	Category
+	Distributor1
+	Distributor2
+)
---
+++ tools/importer/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"io"
+	"log"
+	"os"
+
+	"google.golang.org/protobuf/encoding/prototext"
+
+	dpb "github.com/minkezhang/tracker/api/go/database"
+	entry "github.com/minkezhang/tracker/data/formats/minkezhang"
+)
+
+var (
+	input  = flag.String("input", "/dev/stdin", "input CSV path, e.g. path/to/database.csv")
+	output = flag.String("output", "/dev/stdout", "output textproto path, e.g. path/to/database.textproto")
+)
+
+func main() {
+	flag.Parse()
+
+	fp, err := os.Open(*input)
+	if err != nil {
+		log.Fatalf("cannot open file %v: %v", *input, err)
+	}
+	defer fp.Close()
+
+	scanner := csv.NewReader(fp)
+	if _, err := scanner.Read(); err != nil {
+		log.Fatalf("error while reading CSV file %v: %v", *input, err)
+	}
+
+	var entries []*dpb.Entry
+	for l, err := scanner.Read(); err == nil; l, err = scanner.Read() {
+		r := (*entry.E)(l)
+		if e := r.ProtoBuf(); e.Corpus != dpb.Corpus_CORPUS_UNKNOWN {
+			entries = append(entries, e)
+		}
+	}
+
+	if err != nil && err != io.EOF {
+		log.Fatalf("error while reading CSV file %v: %v", *input, err)
+	}
+
+	db := &dpb.Database{
+		Entries: entries,
+	}
+
+	data, err := prototext.MarshalOptions{
+		Multiline: true,
+	}.Marshal(db)
+	if err != nil {
+		log.Fatalf("error while marshalling proto: %v")
+	}
+
+	w, err := os.Create(*output)
+	if err != nil {
+		log.Fatalf("cannot open output file %v: %v", *output, err)
+	}
+	defer w.Close()
+	if _, err := w.Write(data); err != nil {
+		log.Fatalf("could not write to output file %v: %v", *output, err)
+	}
+}
