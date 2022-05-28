09dee83e883b48bfd53e9f5c0f0ffae84e3ae9df
Minke Zhang <minke.zhang@gmail.com>
2022-05-27 23:52:11 -0700

    Refactor patch into common module

---
+++ tools/cli/commands/patch/common/common.go
@@ -0,0 +1,76 @@
+package common
+
+import (
+	"github.com/minkezhang/tracker/api/go/database/utils"
+	"github.com/minkezhang/tracker/database"
+	"github.com/minkezhang/tracker/tools/cli/commands/get/common"
+	"google.golang.org/protobuf/proto"
+
+	dpb "github.com/minkezhang/tracker/api/go/database"
+	se "github.com/minkezhang/tracker/formats/cli/struct"
+)
+
+type O struct {
+	DB *database.DB
+
+	Title string
+	ID    string
+
+	Body *se.Body
+}
+
+func Patch(opts O) (*dpb.Entry, error) {
+	epb, err := common.Get(common.O{
+		DB:     opts.DB,
+		Title:  opts.Title,
+		ID:     opts.ID,
+		Corpus: opts.Body.GetCorpus(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	opts.Body.SetCorpus(epb.GetCorpus())
+	s, err := opts.Body.Load()
+	if err != nil {
+		return nil, err
+	}
+
+	fpb := s.(*dpb.Entry)
+
+	proto.Merge(epb, fpb)
+	// Ensure we actually replace lists, not the default append as per
+	// default Merge behavior.
+	switch utils.AuxDataL[epb.GetCorpus()] {
+	case utils.AuxDataVideo:
+		if len(fpb.GetAuxDataVideo().GetStudios()) > 0 {
+			epb.GetAuxDataVideo().Studios = fpb.GetAuxDataVideo().GetStudios()
+		}
+		if len(fpb.GetAuxDataVideo().GetWriters()) > 0 {
+			epb.GetAuxDataVideo().Writers = fpb.GetAuxDataVideo().GetWriters()
+		}
+		if len(fpb.GetAuxDataVideo().GetDirectors()) > 0 {
+			epb.GetAuxDataVideo().Directors = fpb.GetAuxDataVideo().GetDirectors()
+		}
+	case utils.AuxDataAudio:
+		if len(fpb.GetAuxDataAudio().GetComposers()) > 0 {
+			epb.GetAuxDataAudio().Composers = fpb.GetAuxDataAudio().GetComposers()
+		}
+	case utils.AuxDataBook:
+		if len(fpb.GetAuxDataBook().GetAuthors()) > 0 {
+			epb.GetAuxDataBook().Authors = fpb.GetAuxDataBook().GetAuthors()
+		}
+	case utils.AuxDataGame:
+		if len(fpb.GetAuxDataGame().GetStudios()) > 0 {
+			epb.GetAuxDataGame().Studios = fpb.GetAuxDataGame().GetStudios()
+		}
+		if len(fpb.GetAuxDataGame().GetStudios()) > 0 {
+			epb.GetAuxDataGame().Studios = fpb.GetAuxDataGame().GetStudios()
+		}
+		if len(fpb.GetAuxDataGame().GetWriters()) > 0 {
+			epb.GetAuxDataGame().Writers = fpb.GetAuxDataGame().GetWriters()
+		}
+	}
+
+	return opts.DB.PutEntry(epb)
+}
