5f110b2d074b3b715cfeb8f3291ab9283fb99a00
Minke Zhang <minke.zhang@gmail.com>
2023-08-06 16:49:54 -0700

    Add sample GraphQL query

--- node/index.js
+++ node/index.js
@@ -2,7 +2,10 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var client_1 = require("@apollo/client");
 var client = new client_1.ApolloClient({
-    uri: "http://localhost:8080/query",
+    uri: 'http://localhost:8080/query',
     cache: new client_1.InMemoryCache(),
 });
-console.log("HELLO WORLD");
+var _Q = (0, client_1.gql)("\n  query {\n    list(input: {\n      title: \"Evangelion\"\n      corpora: [\n        CORPUS_ANIME\n      ]\n      apis: [\n        API_MAL\n      ]\n    }) {\n      metadata {\n        sources {\n          titles {\n            title\n          }\n        }\n      }\n    }\n  }\n");
+client.query({
+    query: _Q,
+}).then(function (result) { return console.log(result); });
--- node/index.ts
+++ node/index.ts
@@ -1,8 +1,32 @@
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: "http://localhost:8080/query",
+  uri: 'http://localhost:8080/query',
   cache: new InMemoryCache(),
 });
 
-console.log("HELLO WORLD")
+const _Q = gql(`
+  query {
+    list(input: {
+      title: "Evangelion"
+      corpora: [
+        CORPUS_ANIME
+      ]
+      apis: [
+        API_MAL
+      ]
+    }) {
+      metadata {
+        sources {
+          titles {
+            title
+          }
+        }
+      }
+    }
+  }
+`)
+
+client.query({
+  query: _Q,
+}).then((result) => console.log(result));
